e4320dc538e2adbcf862c4d2bf28763e71f815b4
AssaupSamir <samirassaup@gmail.com>
2026-04-22 18:17:55 +0300

    page About | navigation burger

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ document.querySelectorAll('.trainer-card').forEach(card => {
         }
     });
 });
+
+
+const burger = document.getElementById('burger');
+const menu = document.getElementById('full-menu');
+const body = document.body;
+burger.addEventListener('click', () => {
+    // Переключаем активный класс у кнопки и у меню
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+    
+    // Запрещаем скролл страницы, когда меню открыто
+    body.classList.toggle('no-scroll');
+});
+
+const links = document.querySelectorAll('.full-menu__list a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        burger.classList.remove('is-active');
+        menu.classList.remove('is-active');
+        body.classList.remove('no-scroll');
+    });
+});
